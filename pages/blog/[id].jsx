2560244ddbb46bfded134542bbe42e136e8ce609
dani.martin <danimartin15@gmail.com>
2022-03-25 23:26:41 +0100

    mi primer commit

---
+++ pages/blog/[id].jsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import Image from "next/image";
+import Layout from "../../components/Layout";
+
+export default function primerPost({data}) {
+  return (
+
+    <Layout>
+      <h1>{data.id} - {data.title} </h1>
+      <Image 
+      src="/img/1.jpg"
+      width={600}
+      height={600}
+      alt="Mi imagen"></Image>
+      <p>{data.body}</p>
+      <Link href="/">
+      <a>Volver</a>
+      </Link>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
+    const paths = data.map(({id}) => ({params:{id: `${id}`}}));
+    return {
+      paths,
+      fallback: false,
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getStaticProps ({params}){
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts/' + params.id);
+    const data = await res.json();
+    return {
+      props: {
+        data
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
---
+++ pages/contact.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+export default function contact() {
+  return (
+
+    <Layout
+    title="Contact"
+    description="Descripcion página contact"
+    >
+        <h1>Contact</h1>   
+    </Layout>
+  )
+}
